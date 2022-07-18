ddf9e84d7b336e513b863be01086f728601911dc
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-07-18 16:01:25 +0600

    -login bg added -logo added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,23 +3,28 @@ import { SiMonster } from 'react-icons/si'
 import { IconContext } from "react-icons";
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+import socialMedia from '../assets/socialMedia.mp4';
 
 const Login = () => {
-useEffect(()=>{
-    let i = 0;
-    setInterval(() => {
-        setColor(colors[++i%colors.length]);
-    }, 700);
-},[]);
-const [color,setColor]= useState("#60a5fa");
-const colors = ["#3b82f6","#2563eb","#1d4ed8"];
 
   return (
-    <div>
-        <IconContext.Provider value={{ color: color, size: "5rem" }}>
-            <SiMonster className="mr-4" />
-        </IconContext.Provider>
+    <div className='flex justify-center items-center flex-col h-screen'>
+        <div className='relative w-full flex justify-center bg-light-black'>
+            <video className='h-screen'
+                src={socialMedia}
+                type="video/mp4"
+                loop
+                controls={false}
+                muted
+                autoPlay
+            />
+            <div className='absolute flex justify-center pt-28 top-0 right-0 left-0 bottom-0 bg-black opacity-50'>
+              <IconContext.Provider value={{ color: "#1d4ed8", size: "5rem" }}>
+                  <SiMonster className="mr-4" />
+              </IconContext.Provider>
+            </div>
+        </div>
+      
     </div>
   )
 }
