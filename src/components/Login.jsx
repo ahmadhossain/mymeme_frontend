0ee6424a7c75d70f69aacf20fa05d19686f361c9
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-07-19 03:00:03 +0600

    google authentication added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { SiMonster } from 'react-icons/si'
-import { IconContext } from "react-icons";
-import { useState } from 'react';
-import { useEffect } from 'react';
-import socialMedia from '../assets/socialMedia.mp4';
+import { IconContext } from "react-icons"
+import { useEffect } from 'react'
+import socialMedia from '../assets/socialMedia.mp4'
+import { gapi } from 'gapi-script'
+import { GoogleLogin } from "react-google-login"
 
 const Login = () => {
+    const clientId = "320321363219-42r6icdjdil9epcq4u2va87gbrrn43v1.apps.googleusercontent.com"
+    const onSuccess= (res) =>{
+        console.log(res.profileObj);
+    }
+    const onFailure = (res) =>{
+        console.log(res);
+    }
+
+    useEffect(()=>{
+      function start(){
+        gapi.client.init({
+          clientId: clientId,
+          scope: ""
+        })
+      }
+      gapi.load('client:auth2',start);
+    },[]);
 
   return (
     <div className='flex justify-center items-center flex-col h-screen'>
@@ -18,13 +36,24 @@ const Login = () => {
                 muted
                 autoPlay
             />
-            <div className='absolute flex justify-center pt-28 top-0 right-0 left-0 bottom-0 bg-black opacity-50'>
-              <IconContext.Provider value={{ color: "#1d4ed8", size: "5rem" }}>
-                  <SiMonster className="mr-4" />
-              </IconContext.Provider>
+            <div className='absolute flex flex-col p-32 items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+              <div className='py-4'>
+                <IconContext.Provider value={{ color: "#3b82f6", size: "5rem" }}>
+                    <SiMonster />
+                </IconContext.Provider>
+              </div>
+              <div id='signInButton' >
+                  <GoogleLogin
+                      clientId = {clientId}
+                      buttonText="Sign in with Google"
+                      onSuccess={onSuccess}
+                      onFailure={onFailure}
+                      cookiePolicy={"single_host_origin"}
+                      isSignedIn={true}
+                  />
+              </div>
             </div>
         </div>
-      
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'light-black':'#040404'
+        'light-black':'#040404',
+        'blackOverlay': 'rgb(0,0,0,0.5)'
       },
     },
   },
