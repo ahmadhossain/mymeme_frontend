80feeba77d8bc61f789d2d2f40c23e2833f53ce2
Ahmad Hossain <ahmadhossain.bu@gmail.com>
2022-08-01 19:33:38 +0600

    -sidebar added -effect issue fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { SiMonster } from 'react-icons/si'
+import { IconContext } from "react-icons"
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll w-50 md:w-60 hide-scrollbar'>
+      <Link
+        to="/"
+        className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+      >
+        <IconContext.Provider value={{ color: "#3b82f6", size: "3.75rem" }}>
+            <SiMonster className='mx-5 cursor-pointer' />
+        </IconContext.Provider>
+      </Link>
+    </div>
   )
 }
 
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -3,7 +3,6 @@ import { useState,useEffect } from 'react'
 import { SiMonster } from 'react-icons/si'
 import { IconContext } from "react-icons"
 import { AiOutlineClose } from "react-icons/ai"
-
 import { GoogleLogout } from "react-google-login"
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
@@ -12,16 +11,21 @@ import Sidebar from '../components/Sidebar'
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
   const navigate = useNavigate();
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  
+  useEffect(()=> {
+      callApi();
+  },[]);
 
-  useEffect( async ()=>{
-    const res = await axios.get(`http://localhost:1337/api/mymeme-users?filters[email][$eq]=${userInfo.email}`);
-    const data = await res.data;
-    console.log(data.data[0].attributes);
-    setUser(data.data[0].attributes);
-    // console.log(userInfo);
-  }, [])
+  const callApi = async () =>{
+      const res = await axios.get(`http://localhost:1337/api/mymeme-users?filters[email][$eq]=${userInfo.email}`);
+      const data = await res.data;
+      // console.log(data.data[0].attributes);
+      // console.log(userInfo);
+      setUser(data.data[0].attributes);
+      console.log(user);
+  }
 
   return (
     <div className='flex bg-gray-100 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
@@ -30,29 +34,31 @@ const Home = () => {
             <Sidebar />
         </div>
         <div className='flex md:hidden flex-row '>
+          <div className='p-3 w-full flex flex-row justify-between items-center shadow-md'>
               {/* Menu icon */}
               <RiMenu5Fill fontSize={40} className="cursor-pointer" onClick={()=> setToggleSidebar(true)} />
               {/* Logo */}
               <Link to="/" >
-                <IconContext.Provider value={{ color: "#3b82f6", size: "3.75rem" }}>
+                <IconContext.Provider value={{ color: "#3b82f6", size: "3.25rem" }}>
                     <SiMonster className='mx-5 cursor-pointer' />
                 </IconContext.Provider>
               </Link>
-              <img src = {user && user.imageUrl} className='rounded-full w-14 h-14' />
-        </div>
-        { toggleSidebar && (
-          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
-            <div className='absolute w-full flex justify-end items-center p-2'>
-              <AiOutlineClose fontSize={30} onClick={()=> setToggleSidebar(false)} />
-            </div>
-            <Sidebar  />
+              <img src = {user && user.imageUrl} className='rounded-full w-12 h-12' />
           </div>
-        )}
-        <GoogleLogout
+          { toggleSidebar && (
+            <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in'>
+              <div className='absolute w-full flex justify-end items-center p-2'>
+                <AiOutlineClose fontSize={30} onClick={()=> setToggleSidebar(false)} />
+              </div>
+              <Sidebar user={ user && user } closeToggle={setToggleSidebar} />
+            </div>
+          )}
+        </div>
+        {/* <GoogleLogout
             clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
             buttonText="Sign out"
-            onLogoutSuccess={()=>{ localStorage.clear(); navigate('/login')}}
-        />
+            onLogoutSuccess={()=>{ navigate('/login', { replace: true})}}
+        /> */}
     </div>
   )
 }
